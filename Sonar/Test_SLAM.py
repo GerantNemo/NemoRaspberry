e05c3ed06dd8ace51b6f63b7606f16e517369575
AlexandreRubenstrunk <alexandre.rubenstrunk@estaca.eu>
2023-06-17 16:19:58 +0200

    Completer la structure pourla fct slam

--- Sonar/Test_SLAM.py
+++ Sonar/Test_SLAM.py
@@ -6,7 +6,13 @@ import os #Incompatibilité avec linux possible ? tester une fonction plus robus
 from sklearn import cluster, datasets
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from sklearn.neighbors import NearestNeighbors
-from Clustering import Clustering_Bassin
+from Clustering import Clustering_Bassin_Polaire,Clustering_Bassin_Carthésien
+from RotationScann import rotationscann
+from Positionnement import DéplacerLeCentre
+from Slam import slam
+
+
+
 
 path = "En_Bas.bin"
 
@@ -44,16 +50,29 @@ lim=0.05
 Nb_Point=3
 Distance_point=2/1024
 
-Sortie_Bas=Traitement_Sonar(Bas,NombreAngle,Distance_point,lim,Nb_Point,NombreAngle)
-Sortie_Haut=Traitement_Sonar(Haut,NombreAngle,Distance_point,lim,Nb_Point,NombreAngle)
+Sortie_Bas=Traitement_Sonar(Bas,Distance_point,lim,Nb_Point,NombreAngle)
+Sortie_Haut=Traitement_Sonar(Haut,Distance_point,lim,Nb_Point,NombreAngle)
 
-theta = np.linspace(0,2*np.pi,NombreAngle)
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-ax.scatter(theta, Sortie_Bas,s=5)
-fig2, ax2 = plt.subplots(subplot_kw={'projection': 'polar'})
-ax2.scatter(theta, Sortie_Haut,s=5)
+OrienterBas=rotationscann(0,Sortie_Bas)                     #Les angles devrons etre déterminer par rapport à la boussole
+OrienterHaut=rotationscann(180,Sortie_Haut)
 
-Mur_Bas,Cluster_Bas,Obstacle_Bas=Clustering_Bassin(Sortie_Bas)
-Mur_Haut,Cluster_Haut,Obstacle_Haut=Clustering_Bassin(Sortie_Haut)
+
+
+TaillePiscineX=2                                            # A Adapter au concour peut etre déterminer grace a un premier scann large
+TaillePiscineY=1
+
+
+
+Mur_Bas,Cluster_Bas,Obstacle_Bas=Clustering_Bassin_Polaire(Sortie_Bas)
+Mur_Haut,Cluster_Haut,Obstacle_Haut=Clustering_Bassin_Polaire(Sortie_Haut)
+
+
+PlacerBas=DéplacerLeCentre(OrienterBas,[0.6,0])               # Les valeur des centre sont à déterminer soit par le sonar soit pas l'imu
+PlacerHaut=DéplacerLeCentre(OrienterHaut,[-0.6,0])
+
+Map=np.concatenate((PlacerBas,PlacerHaut),axis=1)
+plt.figure()
+plt.scatter(Map[0,:],Map[1,:])
+Mur,Cluster,Obstacle=Clustering_Bassin_Carthésien(Map)
 plt.show()
 
