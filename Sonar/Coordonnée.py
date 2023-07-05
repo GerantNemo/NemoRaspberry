91f6963330bbe62dbdf845577158f5d935beab76
AlexandreRubenstrunk <alexandre.rubenstrunk@estaca.eu>
2023-07-05 22:57:28 +0200

    Utilisation de l'algo ransac pour déterminer la regression sur les mur. L'idée est que le scann est découper en 4 on trace une ligne dans chaque quart et on regarde combien de point on été pris en compte. ON recommande en décalant de 1 gradiant les quarter et on compart en gardant celui qui a le plus de point

--- Sonar/Coordonnée.py
+++ Sonar/Coordonnée.py
@@ -19,38 +19,62 @@ def Coodonnée_Nautilus(Scann):            #A faire intervenir apres l'orientati
     """ Permet de placer la position du sous marin dans le bassin en utilisant les murs comme reférences
     Scann: Clustering des mur réaliser grace a la fonction cluster"""                    
     #Pour etre sur de l'avoire en carthésien
-    ScannCarthésien=np.zeros((len(Scann),2))
+    ScannCartesien=np.zeros((len(Scann),2))
     if Scann.shape[1]==1:
         for i in range(len(Scann)):
-            ScannCarthésien[i,0]=Scann[i]*math.cos(i*2*math.pi/400)
-            ScannCarthésien[i,1]=Scann[i]*math.sin(i*2*math.pi/400)
+            ScannCartesien[i,0]=Scann[i]*math.cos(i*2*math.pi/400)
+            ScannCartesien[i,1]=Scann[i]*math.sin(i*2*math.pi/400)
     else:
-        ScannCarthésien=Scann
+        ScannCartesien=Scann
     
     
     # Définir les paramètres de RANSAC
-    min_samples = 100  # Nombre minimum de points pour former une ligne
+    min_samples = 25  # Nombre minimum de points pour former une ligne
     max_trials = 100  # Nombre d'itérations de RANSAC
     distance_threshold = 0.05  # Distance seuil pour considérer un point comme faisant partie d'une ligne
-
+    plt.figure()
     # Appliquer RANSAC
     # Normalement on peut faire une regression linéaire quartier par quartier pour trouver les lignes puis comparer les equation de droite pour voir les meilleur
+    inlinerMax=0
+    inlinerSomme=0
+    for j in range(int((len(ScannCartesien)/4)-1)):
+        print(j)
+        print(len(ScannCartesien))
+        for i in range(0,3):
+            ScannRansac=ScannCartesien[int((((i)*len(ScannCartesien)/4)+j)%len(ScannCartesien)):int((((i+1)*len(ScannCartesien)/4)+j)%len(ScannCartesien)), :]
+            ransac = RANSACRegressor(min_samples=min_samples,
+                                    residual_threshold=distance_threshold,
+                                    max_trials=max_trials)
+            ransac.fit(ScannRansac[:, 0].reshape(-1, 1), ScannRansac[:, 1])  # Régression linéaire
 
-    ransac = RANSACRegressor(min_samples=min_samples,
-                            residual_threshold=distance_threshold,
-                            max_trials=max_trials)
-    ransac.fit(ScannCarthésien[:, 0].reshape(-1, 1), ScannCarthésien[:, 1])  # Régression linéaire
+            # Obtenir les inliers (points valides) de la meilleure ligne trouvée
+            inlier_mask = ransac.inlier_mask_
+            inliers = ScannRansac[inlier_mask]
+            inlinerSomme=inlinerSomme+len(inliers)
+            if i==2:
+                if inlinerSomme>inlinerMax:
+                    indiceJ=j
+                    inlinerMax=inlinerSomme
+                inlinerSomme=0
+            
+                
+    for i in range(0,3):
+        ScannRansac=ScannCartesien[int((((i)*(len(ScannCartesien))/4)+indiceJ)):int((((i+1)*(len(ScannCartesien))/4)+indiceJ)), :]
+        ransac = RANSACRegressor(min_samples=min_samples,
+                                    residual_threshold=distance_threshold,
+                                    max_trials=max_trials)
+        ransac.fit(ScannRansac[:, 0].reshape(-1, 1), ScannRansac[:, 1])  # Régression linéaire
 
-    # Obtenir les inliers (points valides) de la meilleure ligne trouvée
-    inlier_mask = ransac.inlier_mask_
-    inliers = ScannCarthésien[inlier_mask]
+        # Obtenir les inliers (points valides) de la meilleure ligne trouvée
+        inlier_mask = ransac.inlier_mask_
+        inliers = ScannRansac[inlier_mask]
 
+        plt.scatter(inliers[:, 0], inliers[:, 1], color='r', label='Inliers')
+        plt.plot(ScannRansac[:, 0], ransac.predict(ScannRansac[:, 0].reshape(-1, 1)), color='g', label='Ligne détectée')
     # Tracer les lignes détectées
-    import matplotlib.pyplot as plt
+    
+    plt.scatter(ScannCartesien[:, 0], ScannCartesien[:, 1], color='b', label='Points')
 
-    plt.scatter(ScannCarthésien[:, 0], ScannCarthésien[:, 1], color='b', label='Points')
-    plt.scatter(inliers[:, 0], inliers[:, 1], color='r', label='Inliers')
-    plt.plot(ScannCarthésien[:, 0], ransac.predict(ScannCarthésien[:, 0].reshape(-1, 1)), color='g', label='Ligne détectée')
     plt.legend()
     plt.show()
     return()
